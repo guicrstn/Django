679bbee08dabf43c53805e99c1fc95b996e1e416
guicrstn <gcristini@app.afpma.fr>
2022-06-20 10:48:22 +0200

    update interface

--- computer/models.py
+++ computer/models.py
@@ -1,9 +1,25 @@
 from django.db import models
 
 class Interface(models.Model):
-    addr = models.CharField(max_length=255)
+    addr    = models.CharField(max_length=255,default=None)
+    IP      = models.CharField(max_length=255,default=None)
+    Subnet  = models.CharField(max_length=255,default=None)
+    Gateway = models.CharField(max_length=255,default=None)
+    Mac     = models.CharField(max_length=255,default=None)
+
     def __str__(self) -> str:
+        return f'{self.addr}, {self.IP}, {self.Subnet}, {self.Gateway}, {self.Mac}'
+
+    def getaddr(self):
         return self.addr
+    def getip(self):
+        return self.IP
+    def getsubnet(self):
+        return self.Subnet
+    def getgateway(self):
+        return self.Gateway
+    def getmac(self):
+        return self.Mac
 
 
 class Computer(models.Model):
