c7f0dbc8c99abe64f0bec35106554b009152e03e
guicrstn <gcristini@app.afpma.Fr>
2022-10-25 10:08:54 +0200

    modif admin

--- computer/admin.py
+++ computer/admin.py
@@ -1,7 +1,11 @@
-from multiprocessing.connection import Client
 from django.contrib import admin
-from computer.models import Computer, Interface, Client
 
-admin.site.register(Computer)
-admin.site.register(Interface)
-admin.site.register(Client)
+from computer.models import Client
+
+class ClientAdmin(admin.ModelAdmin):
+    '''
+    Classe de personnalisation du formulaire et table du champ d'administration d'un client.
+    '''
+    pass # TODO : Afficher le nom et la date d'ajout du client
+
+admin.site.register(Client,ClientAdmin)
