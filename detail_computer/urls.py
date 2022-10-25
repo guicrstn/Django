9b79171a8dfe37c00fc43fda61197b563fbffc18
guicrstn <gcristini@app.afpma.Fr>
2022-10-25 10:10:24 +0200

    modif urls

--- detail_computer/urls.py
+++ detail_computer/urls.py
@@ -1,4 +1,4 @@
-"""detail_computer URL Configuration
+"""project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('', include('computer.urls')),
     path('admin/', admin.site.urls),
+    path("__reload__/", include("django_browser_reload.urls")),
+    path('api-auth/', include('rest_framework.urls'))
 ]
