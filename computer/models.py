a9cc21d07daa1d42e11f515a8f2d909dcd936cad
guicrstn <gcristini@app.afpma.Fr>
2022-10-25 10:07:03 +0200

    modif models

--- computer/models.py
+++ computer/models.py
@@ -1,49 +1,23 @@
-from unicodedata import name
-from django.db import models
 
+from django.db import models
 
 class Interface(models.Model):
     '''
-    Class: Interface
-    Description: Classe qui permet de repertorié:
-    - Nom
-    - Addr
-    - IP
-    - Masque de sous réseaux
-    - Passerelle
-    - Adresse mac
+    Interface concerant la connexion d'un ordinateur.
     '''
     name    = models.CharField(max_length=255,default=None)
     addr    = models.CharField(max_length=255,default=None)
-    IP      = models.CharField(max_length=255,default=None)
-    Subnet  = models.CharField(max_length=255,default=None)
-    Gateway = models.CharField(max_length=255,default=None)
-    Mac     = models.CharField(max_length=255,default=None)
+    ip      = models.CharField(max_length=255,default=None)
+    dubnet  = models.CharField(max_length=255,default=None)
+    gateway = models.CharField(max_length=255,default=None)
+    mac     = models.CharField(max_length=255,default=None)
 
     def __str__(self) -> str:
-        return f'{self.name}'
-    def getaddr(self):
-        return self.addr
-    def getip(self):
-        return self.IP
-    def getsubnet(self):
-        return self.Subnet
-    def getgateway(self):
-        return self.Gateway
-    def getmac(self):
-        return self.Mac
-    def getname(self):
         return self.name
-
 
 class Computer(models.Model):
     '''
-    Class: Computer
-    Description: Classe qui permet de repertorié:
-    - Nom
-    - Espace disque total
-    - Espace disque restant 
-    - Espace disque utilisé 
+    Ordinateur qui contient une interface réseau et des information sur l'espace de stockage.
     '''
     name    = models.CharField(max_length=255,default=None)
     total   = models.FloatField(default=None)
@@ -52,19 +26,17 @@ class Computer(models.Model):
         
     def __str__(self) -> str:
         return '{}, {}, {}, {}'.format(self.name, self.total, self.free, self.used)
-    def totaldisk(self):
-        return self.total // (2**30)
-    def useddisk(self):
-        return self.used // (2**30)
-    def freedisk(self):
-        return self.free // (2**30)
-    def getname(self):
-        return self.name
 
 class Client(models.Model):
+    '''
+    Client qui contient des ordinateurs.
+    '''
     name    = models.CharField(max_length=255,default=None)
-    username= models.CharField(max_length=255,default=None)
-    def getname(self):
+    address = models.CharField(max_length=255,default=None)
+    zip     = models.CharField(max_length=255,default=None)
+    city    = models.CharField(max_length=255,default=None)
+    phone   = models.CharField(max_length=255,default=None)
+    mail    = models.CharField(max_length=255,default=None)
+
+    def __str__(self) -> str:
         return self.name
-    def getname(self):
-        return self.username
